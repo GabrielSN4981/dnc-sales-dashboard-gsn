a7e6a4f023fcef4aad884504d2eef3a59455c36d
Gabriel <gabrielsantosnogueira2005@gmail.com>
2025-06-16 23:49:54 -0300

    componente de avatarList

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,33 @@
-import { Header, CardComponent } from "@/components";
+import { Header, CardComponent, AvatarList } from "@/components";
 import { Container } from "@mui/material";
+import { currencyConverter } from "@/utils";
 
 function Home() {
+  const mockListData = [
+    {
+      avatar: "/dnc-avatar.svg",
+      name: "Nome Sobrenome 1",
+      subtitle: currencyConverter(1000),
+    },
+    {
+      avatar: "/dnc-avatar.svg",
+      name: "Nome Sobrenome 2",
+      subtitle: currencyConverter(2000),
+    },
+    {
+      avatar: "/dnc-avatar.svg",
+      name: "Nome Sobrenome 3",
+      subtitle: currencyConverter(3000),
+    },
+  ];
   return (
     <>
       <Header />
       <Container maxWidth="lg">
-          <CardComponent>CARD</CardComponent>
+        <CardComponent>CARD</CardComponent>
+        <CardComponent>
+          <AvatarList listData={mockListData} />
+        </CardComponent>
       </Container>
     </>
   );
